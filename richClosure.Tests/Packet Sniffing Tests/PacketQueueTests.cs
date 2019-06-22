cbeb22eff6a1ce30c96fb10565959e70d1fe1a0b
Arkadiusz Kołodziejek <arkko002@gmail.com>
2019-06-22 21:58:09 +0200

    Add PacketQueueTests, SnifferThreadsTests.cs

---
+++ richClosure.Tests/Packet Sniffing Tests/PacketQueueTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using richClosure.Packet_Sniffing;
+
+namespace richClosure.Tests.Packet_Sniffing_Tests
+{
+    [TestFixture]
+    class PacketQueueTests
+    {
+        private PacketQueue _queue;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _queue = new PacketQueue();
+        }
+
+        [Test]
+        public void AddAndRemoveItem_ReturnsItem()
+        {
+            byte[] byteArr = {1, 2};
+
+            _queue.EnqueuePacket(byteArr);
+            byte[] result = _queue.DequeuePacket();
+
+            Assert.AreEqual(byteArr, result);
+        }
+    }
+}
